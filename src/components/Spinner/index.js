be59c2fc6c8f21f4dab8d860b0d8df42c3fe1722
QuangThai <nguyenquangthaib1@gmail.com>
2021-07-21 11:54:13 +0700

    fix eslint

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./styles.css";
 
 function Spinner() {
-  return <div className="fade-loading"></div>;
+  return <div className="fade-loading" />;
 }
 
 export default Spinner;
